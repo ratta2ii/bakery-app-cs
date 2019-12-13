e79e235471a77e299fb5829c8d65d777ec4eda68
Trevor Mackin <tismetrm@gmail.com>
2019-12-13 08:48:23 -0800

    Create project directories and files. Add boilerplate to Program.csproj. Add directors to both Program.cs and MenuItems.cs. Add a namespace in each of the 2 files. Add 2 separate classes, one for Bread, another name Pastry.

---
+++ Menu/SaleItems.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Menu.SaleItems {
+    class Bread {
+       
+    }
+
+    class Pastry {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Menu.SaleItems {
+    class Program {
+        static void Main()
+        {
+            
+        }
+    }
+}
