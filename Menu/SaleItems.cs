733ad2610ea3b117bd68c2ab9620fc98628211ed
Trevor Mackin <tismetrm@gmail.com>
2019-12-13 09:22:34 -0800

    Ask the user to enter the total number of both pastries and bread into the console. Capture these the entries and save them to variables to be used to calculate the total costs.

--- Menu/SaleItems.cs
+++ Menu/SaleItems.cs
@@ -8,5 +8,14 @@ namespace Menu.SaleItems {
 
     class Pastry {
 
+        public string PastryName { get; set; }
+        public int PastryCount { get; set; }
+
+        public Pastry(string name, int pastryCount)
+        {
+            PastryName = name;
+            PastryCount = pastryCount;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,16 @@ namespace Menu.SaleItems {
     class Program {
         static void Main()
         {
-            
+           Console.WriteLine("Welcome to May's, the #1 Bakery in the County.");
+           Console.WriteLine("Please Choose the total number of pastries you would like to order, followed by the best Bread ever. ");
+
+           Console.WriteLine("How many pastries would you like to order?");
+           int numberOfPastries = int.Parse(Console.ReadLine());
+
+           Console.WriteLine($"Looks like you chose {numberOfPastries}, great. Now how many pieces fo bread would you like with that?");
+
+           int numberOfBread = int.Parse(Console.ReadLine());
+           Console.WriteLine($"Great, looks like you chose {numberOfBread}. Let me get your total for you.");
         }
     }
 }
