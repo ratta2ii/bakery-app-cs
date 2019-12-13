3a15d5790302efba8cbde9ffba501a313b7cc4d0
Trevor Mackin <tismetrm@gmail.com>
2019-12-13 15:38:55 -0800

    Clean up indentation. Research markup language and how to utilize various tools.

--- Program.cs
+++ Program.cs
@@ -6,20 +6,17 @@ namespace Menu.Order {
         static void Main()
         {
             Console.WriteLine("Welcome to Pierre's, the #1 Bakery in the County.");
-            Console.WriteLine("--------------------");
-
             Console.WriteLine("How many pastries would you like to order?");
             int numberOfPastries = int.Parse(Console.ReadLine());
-
             Console.WriteLine("How many pieces of loaves of bread would you like to order?");
             int numberOfBread = int.Parse(Console.ReadLine());
+            
             Console.WriteLine("--------------------");
-
             Console.WriteLine($"It looks like you chose {numberOfPastries} from pastries, and {numberOfBread} from bread.");
 
             Order customerOrder = new Order(numberOfPastries, numberOfBread);
 
-            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("--------------------");
             Console.WriteLine(customerOrder.TotalCosts(customerOrder.PastryCount, customerOrder.BreadCount));
         }
     }
